2807766c213f57b4f95d5cf7efda48225cd81726
Britta Carlsson <47212060+BritneyBob@users.noreply.github.com>
2019-02-26 00:38:36 +0100

    Fixed lab3

--- vxml-server/app.py
+++ vxml-server/app.py
@@ -46,13 +46,6 @@ def lab2():
     return response
 
 @app.route('/lab3')
-def lab2():
-    vxml = render_template('lab3.xml')
-    response = make_response(vxml)
-    response.headers["Content-Type"] = "application/xml"
-    return response
-
-@app.route('/lab3')
 def lab3():
     vxml = render_template('lab3.xml')
     response = make_response(vxml)
